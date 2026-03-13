8a8e8b5349d43c3b155e5096d635b77f48c7a1aa
Snehodipto Das <dsnehodipto@gmail.com>
2026-03-13 23:00:03 +0530

    Deploying server with final fixes

--- server/app.py
+++ server/app.py
@@ -4,7 +4,11 @@ from routes.weather import router as weather_router
 app = FastAPI(title="AeroSentra")
 
 app.include_router(weather_router, prefix="/api")
-# app.mount("/socket.io", sio_app)
+
+@app.get("/healthz")
+def health():
+    return "Server Running OK ✅"
+
 @app.get("/")
 def root():
     return { 
